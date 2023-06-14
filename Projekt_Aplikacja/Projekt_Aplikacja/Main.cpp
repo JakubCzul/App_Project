a18e3c9a05fc125b6a5a540f18f5dc4a50350f98
JakubCzul <jc305354@student.polsl.pl>
2023-06-14 15:21:33 +0200

    Początki

---
+++ Projekt_Aplikacja/Projekt_Aplikacja/Main.cpp
@@ -0,0 +1,19 @@
+#include "Function.h"
+#include "SDL.h"
+
+int main(int argc, char* argv[]) {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+
+	SDL_RenderClear(render);
+	
+	SDL_RenderPresent(render);
+
+	SDL_Delay(3000);
+
+	return 0;
+}
